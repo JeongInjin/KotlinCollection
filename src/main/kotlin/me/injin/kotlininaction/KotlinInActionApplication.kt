4a70c02eeed86d39a314a9033b780f3fe5e358c1
injinjeong <snowwwwww@naver.com>
2022-09-08 19:48:43 +0900

    first commit

---
+++ src/main/kotlin/me/injin/kotlininaction/KotlinInActionApplication.kt
@@ -0,0 +1,11 @@
+package me.injin.kotlininaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinInActionApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinInActionApplication>(*args)
+}
