a7e84feba8c931d717b79e89ff1e28cc36b9d2be
injinjeong <snowwwwww@naver.com>
2022-09-24 10:12:33 +0900

    test code 수정

--- src/main/kotlin/me/injin/util/StringUtils.kt
+++ src/main/kotlin/me/injin/util/StringUtils.kt
@@ -1,5 +1,11 @@
 package me.injin.util
 
+const val REQUIRED_NAME = "성함은 필수 입니다."
+
+//아래 enum 클래스 와 함수는 테스트용 이므로, 혹시 사용할 경우 package 별 분리를 권장합니다.
+enum class REGEX_TYPE(val pattern: String) {
+    KOREAN_WITH_SPACES("^[가-힣\\s]*$")
+}
 fun throwBusinessException(msg: String?): Nothing {
     throw IllegalArgumentException(msg?: "")
 }
@@ -11,23 +17,22 @@ fun throwBusinessException(msg: String?): Nothing {
  * @see (필수): 본 함수는 [읿반 유저] 대상으로 진행됩니다.
  * @see (ko): [가-힣 까지의 한글 + 공뱍 허용] 조건을 검색합니다.
  */
-fun String.RegularExpressionCheck(regularExpressionTargetType: String): Boolean {
-    return when (regularExpressionTargetType.toUpperCase()) {
-        "KO" -> Regex("^[가-힣\\s]*$").matches(this)
-        else -> {false}
+infix fun String.RegularExpressionCheck(regularExpressionTargetType: REGEX_TYPE): Boolean {
+    return when (regularExpressionTargetType) {
+        REGEX_TYPE.KOREAN_WITH_SPACES -> Regex(REGEX_TYPE.KOREAN_WITH_SPACES.pattern).matches(this)
     }
 }
 
 /**
  * 정규식을 검사한뒤, target 을 replaceText 치환 합니다.
  * 정규식 조건에 포함되지 않으면 trim() 만 수행합니다.
- * @return replace 된 String 객체.
- * @throws trim() 조건후 null or empty 일 경우 IllegalArgumentException
+ * @return :replace 된 String 객체.
+ * @throws :trim() 조건후 null or empty 일 경우 throwBusinessException
  */
-fun String.ReplaceTargetStringOrTrim(regexType: String, target: String, replaceText: String): String {
-    if(this.trim().isNullOrEmpty()) throwBusinessException("고객 성함은 필수 입니다.")
-    if(!this.RegularExpressionCheck(regexType)) return this.trim()
-    return this.replace(target, replaceText)
+fun String.ReplaceTargetStringOrTrim(regexType: REGEX_TYPE, target: String, replaceText: String): String {
+    if(this.trim().isNullOrEmpty()) throwBusinessException(REQUIRED_NAME)
+    return if(this RegularExpressionCheck regexType) this.replace(target, replaceText)
+    else this.trim()
 }
 
 class StringUtils {
--- src/test/kotlin/me/injin/kotlininaction/etc/RegexTest.kt
+++ src/test/kotlin/me/injin/kotlininaction/etc/RegexTest.kt
@@ -1,12 +1,11 @@
 package me.injin.kotlininaction.etc
 
+import me.injin.util.REGEX_TYPE.KOREAN_WITH_SPACES
 import me.injin.util.ReplaceTargetStringOrTrim
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-private const val KOREAN_PATTERNS_WITH_SPACES = "^[가-힣\\s]*$"
-private const val KO = "ko"
 private const val GAP = " "
 class RegexTest {
 
@@ -22,19 +21,17 @@ class RegexTest {
     @Test
     @DisplayName("한글만 존재하는 정규식을 테스트 합니다.")
     fun RegularExpressionCheckTest1() {
-        //given
-
-        //when
-        val matches0 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("ㄱㄱㄱ")
-        val matches1 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("ㄱ나니")
-        val matches2 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("고ㅎ장")
-        val matches3 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("고흐ㅎ")
-        val matches4 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("고흐ㅎ ")
-        val matches5 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(" ㅁ고흐")
-        val matches6 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("1정인진")
-        val matches7 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("정인진2")
-        val matches8 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("정1인진")
-        val matches9 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("궭뷁솷")
+        //given//when
+        val matches0 = Regex(KOREAN_WITH_SPACES.pattern).matches("ㄱㄱㄱ")
+        val matches1 = Regex(KOREAN_WITH_SPACES.pattern).matches("ㄱ나니?")
+        val matches2 = Regex(KOREAN_WITH_SPACES.pattern).matches("고ㅎ장")
+        val matches3 = Regex(KOREAN_WITH_SPACES.pattern).matches("고흐ㅎ")
+        val matches4 = Regex(KOREAN_WITH_SPACES.pattern).matches("고흐ㅎ ")
+        val matches5 = Regex(KOREAN_WITH_SPACES.pattern).matches(" ㅁ고흐")
+        val matches6 = Regex(KOREAN_WITH_SPACES.pattern).matches("1정인진")
+        val matches7 = Regex(KOREAN_WITH_SPACES.pattern).matches("정인진2")
+        val matches8 = Regex(KOREAN_WITH_SPACES.pattern).matches("정1인진")
+        val matches9 = Regex(KOREAN_WITH_SPACES.pattern).matches("궭뷁솷")
 
         val oldMatches1 = Regex("^[가-힣|/s]+$").matches(testName0) //기존 정규식.
         val oldMatches2 = Regex("^[가-힣|/s]+$").matches(testName1) //기존 정규식. 공백 존재하면 실패
@@ -60,17 +57,15 @@ class RegexTest {
     @Test
     @DisplayName("한글만 존재하는 정규식을 테스트 합니다.")
     fun RegularExpressionCheckTest2() {
-        //given
-
-        //when
-        val matches0 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches("정인진")
-        val matches1 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName1)
-        val matches2 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName2)
-        val matches3 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName3)
-        val matches4 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName4)
-        val matches5 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName5)
-        val matches6 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName6)
-        val matches7 = Regex(KOREAN_PATTERNS_WITH_SPACES).matches(testName7)
+        //given//when
+        val matches0 = Regex(KOREAN_WITH_SPACES.pattern).matches("정인진")
+        val matches1 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName1)
+        val matches2 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName2)
+        val matches3 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName3)
+        val matches4 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName4)
+        val matches5 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName5)
+        val matches6 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName6)
+        val matches7 = Regex(KOREAN_WITH_SPACES.pattern).matches(testName7)
 
         //then
         assertThat(matches0).isTrue
@@ -86,27 +81,25 @@ class RegexTest {
     @Test
     @DisplayName("한글만 존재하는 정규식 테스트가 통과하면 치환 합니다.")
     fun `Extension Function Test`() {
-        //given
+        //given//when
+        val test1 = "정인진".ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val test2 = " 정인진".ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val test3 = "정인진 ".ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val test4 = "정 인 진 ".ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
 
-        //when
-        val test1 = "정인진".ReplaceTargetStringOrTrim(KO, GAP, "")
-        val test2 = " 정인진".ReplaceTargetStringOrTrim(KO, GAP, "")
-        val test3 = "정인진 ".ReplaceTargetStringOrTrim(KO, GAP, "")
-        val test4 = "정 인 진 ".ReplaceTargetStringOrTrim(KO, GAP, "")
+        val resultName0 = testName0.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val resultName1 = testName1.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val resultName2 = testName2.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val resultName3 = testName3.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val resultName4 = testName4.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
 
-        val resultName0 = testName0.ReplaceTargetStringOrTrim(KO, GAP, "")
-        val resultName1 = testName1.ReplaceTargetStringOrTrim(KO, GAP, "")
-        val resultName2 = testName2.ReplaceTargetStringOrTrim(KO, GAP, "")
-        val resultName3 = testName3.ReplaceTargetStringOrTrim(KO, GAP, "")
-        val resultName4 = testName4.ReplaceTargetStringOrTrim(KO, GAP, "")
+        val resultName5 = testName5.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val resultName6 = testName6.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
+        val resultName7 = testName7.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "")
 
-        val resultName5 = testName5.ReplaceTargetStringOrTrim(KO, GAP, "")
-        val resultName6 = testName6.ReplaceTargetStringOrTrim(KO, GAP, "")
-        val resultName7 = testName7.ReplaceTargetStringOrTrim(KO, GAP, "")
-
-        val resultName8 = testName2.ReplaceTargetStringOrTrim(KO, GAP, "-")
-        val resultName9 = testName3.ReplaceTargetStringOrTrim(KO, GAP, "-")
-        val resultName10 = testName4.ReplaceTargetStringOrTrim(KO, GAP, "-")
+        val resultName8 = testName2.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "-")
+        val resultName9 = testName3.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "-")
+        val resultName10 = testName4.ReplaceTargetStringOrTrim(KOREAN_WITH_SPACES, GAP, "-")
         //then
         assertThat(test1).isEqualTo("정인진")
         assertThat(test2).isEqualTo("정인진")
@@ -128,4 +121,5 @@ class RegexTest {
         assertThat(resultName9).isEqualTo("-정-인진-")
         assertThat(resultName10).isEqualTo("정-인-진-")
     }
+
 }
