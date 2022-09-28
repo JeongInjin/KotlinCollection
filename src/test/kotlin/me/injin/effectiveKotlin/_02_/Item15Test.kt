edc411e556f3e42ad419f9a1e0894bdfad357bea
injinjeong <snowwwwww@naver.com>
2022-09-28 13:00:55 +0900

    effective kotlin

--- src/test/kotlin/me/injin/effectiveKotlin/_02_/Item15Test.kt
+++ src/test/kotlin/me/injin/effectiveKotlin/_02_/Item15Test.kt
@@ -1,4 +1,21 @@
 package me.injin.effectiveKotlin._02_
 
+import org.junit.jupiter.api.Test
+
 class Item15Test {
+
+    @Test
+    fun Test() {
+        val node = Node("parent")
+        node.makeChild("child")
+    }
+}
+
+class Node(val name: String) {
+    fun makeChild(childName: String) =
+        create("$name.$childName")
+//            .apply { print("Created ${this?.name}") }
+            .let { print("Created ${it?.name}") }
+
+    private fun create(name: String): Node? = Node(name)
 }
