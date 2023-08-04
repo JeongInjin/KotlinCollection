f629acf9fdcc02f42f69a8addbfeb3af31252e91
JeongInjin <snowwwwww@naver.com>
2023-08-04 13:40:11 +0900

    이펙티브 코틸른 아이템2

--- src/test/kotlin/me/injin/effectiveKotlin/_01_/Item2Test.kt
+++ src/test/kotlin/me/injin/effectiveKotlin/_01_/Item2Test.kt
@@ -1,22 +1,63 @@
 package me.injin.effectiveKotlin._01_
 
+import me.injin.extTest.Color
 import org.junit.jupiter.api.Test
 
 class Item2Test {
 
     @Test
+    fun `변수를 정의할 때 초기화되는 것이 좋다`() {
+        // 나쁜 예
+//        val user: User
+//        if (hasValue) {
+//            user = getValue()
+//        } else {
+//            user = User()
+//        }
+
+        //조금 더 좋은 예
+//        val user = User = if (hasValue) {
+//            getValue()
+//        } else {
+//            User()
+//        }
+
+    }
+
+    @Test
     fun `구조 분해 선언 테스트 - destructuring declaration`() {
         val updateWeather = updateWeather(4)
+        val updateWeatherBad = updateWeatherBadExample(15)
         val (desc, color) = updateWeather(15)
         println("updateWeather.first: ${updateWeather.first}, updateWeather.second: ${updateWeather.second}")
+        println("updateWeatherBad.first: ${updateWeatherBad.first}, updateWeatherBad.second: ${updateWeatherBad.second}")
         println("desc: $desc, color: $color")
     }
 
-    fun updateWeather(degrees: Int): Pair<String, String> {
+    //나쁜 예
+    fun updateWeatherBadExample(degrees: Int): Pair<String, Color> {
+        val description: String
+        val color: Color
+        if (degrees < 5) {
+            description = "cold"
+            color = Color.BLUE
+        } else if (degrees < 23) {
+            description = "mild"
+            color = Color.YELLOW
+        } else {
+            description = "hot"
+            color = Color.RED
+        }
+
+        return description to color
+    }
+
+    // 조금 더 좋은 예
+    fun updateWeather(degrees: Int): Pair<String, Color> {
         val (description, color) = when {
-            degrees < 5 -> "cold" to "blue"
-            degrees < 23 -> "middle" to "yello"
-            else -> "hot" to "red"
+            degrees < 5 -> "cold" to Color.BLUE
+            degrees < 23 -> "mild" to Color.YELLOW
+            else -> "hot" to Color.RED
         }
         println("description: $description, color: $color")
         return (description to color)
@@ -42,7 +83,7 @@ class Item2Test {
     fun `에라토스체네스의 체 sequence 이용`() {
         val primes: Sequence<Int> = sequence {
             //2부터 검색하기위해 인수로 2를 주고 1씩 증가하도록 시퀀스 정의
-            var numbers = generateSequence(2) { it + 1}
+            var numbers = generateSequence(2) { it + 1 }
 
             while (true) {
                 val prime = numbers.first()
@@ -60,19 +101,19 @@ class Item2Test {
         val test = (100..200).map { it * 2 }
             .filter { it % 3 == 0 } // 여기서 101번의 불필요한 연산이 발생하게된다.
             .first()
-        println(test)
+        println("test: $test")
 
         // 총 104번 연산
         val test2 = (100..200).map { it * 2 }
             .first { it % 3 == 0 } // 조건을 만족하는 경우 이후 원소는 불필요한 연산을 수행하지 않게된다.
-        println(test2)
+        println("test2: $test2")
 
         val test3 = (100..2_000_000).asSequence() // 주목할만한 점은 200만으로 올렸음에도 실행속도에 영향이 없다.
             .map { println("doubling $it"); it * 2 }
             .filter { println("filtering $it"); it % 3 == 0 }
 //            .first() // 시퀀스가 비었다면 NoSuchElementException이 발생한다.
             .firstOrNull()
-        println(test3)
+        println("test3: $test3")
 
     }
 
@@ -93,10 +134,11 @@ class Item2Test {
         list2 += 1
 
         list1 += 2 // list1.plusAssign(2)로 변경됨.
-        list2 +=2 //list2.plus(2)로 변경됨.
+        list2 += 2 //list2.plus(2)로 변경됨.
 
         list1.plusAssign(3)
         list2.plus(3)
     }
 }
+
 data class CopyTest(val name: String)
