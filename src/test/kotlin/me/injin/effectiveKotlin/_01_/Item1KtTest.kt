2f5139069e158cf2e9caa677ecef4bca35e5b1ef
JeongInjin <snowwwwww@naver.com>
2024-03-24 23:15:36 +0900

    effectiveKOtlin

--- src/test/kotlin/me/injin/effectiveKotlin/_01_/Item1KtTest.kt
+++ src/test/kotlin/me/injin/effectiveKotlin/_01_/Item1KtTest.kt
@@ -13,6 +13,11 @@ class Item1KtTest {
 
     @Test
     fun p6_1() {
+
+        val list = mutableListOf(1, 2, 3)
+//        list = mutableListOf(4, 5, 6) //에러
+
+
         var num = 0
         repeat(1000) {
             thread {
@@ -70,6 +75,39 @@ class Item1KtTest {
         println(fullName) // Kim InJin
     }
 
+    // p9_1
+    interface Element {
+        val active: Boolean
+    }
+
+    // 함수 본문에서 override
+    class ActualElement : Element {
+        override var active: Boolean = false
+    }
+
+    // 생성자에서 override
+    class ActualElement2(override val active: Boolean) : Element
+
+    @Test
+    fun p9_1() {
+        val element = ActualElement()
+        element.active = true
+        println(element.active)
+
+        val element2 = ActualElement2(true)
+        println(element2.active)
+    }
+
+    @Test
+    fun p10_1() {
+        fun printLength(obj: Any) {
+            if (obj is String) {
+                // obj는 자동으로 String 타입으로 캐스트됩니다.
+                println(obj.length) // 스마트 캐스트
+            }
+        }
+    }
+
     @Test
     fun p12_1() {
         val list = listOf(1, 2, 3)
@@ -83,6 +121,77 @@ class Item1KtTest {
         mutableList.add(4)
         assertThat(mutableList).hasSize(4)
     }
+
+    @Test
+    fun p13_1() {
+        val list = listOf(1, 2, 3)
+        val mutableList = list.toMutableList()
+        mutableList.add(4)
+        assertThat(mutableList).hasSize(4)
+        assertThat(list).hasSize(3)
+    }
+
+    @Test
+    fun p12_2() {
+        val list = listOf(1, 2, 3)
+        // list.add(4) - 에러
+        if (list is MutableList) {
+            list.add(4) // 이렇게 하지마시오
+        }
+    }
+
+    data class Person_p13(val name: String, val surname: String)
+
+    @Test
+    fun `p13 4번 - 방어적복사 + 깊은복사 필요가없다`() {
+        val originPerson = Person_p13("Injin", "Jeong")
+        val copyPerson = originPerson
+//        originPerson.name = "minsu" // 에러
+//        originPerson.surname = "Kim" // 에러
+        assertThat(originPerson).isEqualTo(copyPerson)
+
+        val sharedPerson = originPerson
+//        sharedPerson.name = "minsu" // 에러
+//        sharedPerson.surname = "Kim" // 에러
+        assertThat(originPerson).isEqualTo(sharedPerson)
+    }
+
+    data class PersonWithFriends(
+        val name: String,
+        val surname: String,
+        val friends: MutableList<String> = mutableListOf()
+    )
+
+    @Test
+    fun `p13 4번 - 방어적복사 + 깊은복사 예외케이스`() {
+        val originPerson = PersonWithFriends("Injin", "Jeong")
+        originPerson.friends.add("minsu")
+
+        val copyPerson = originPerson.copy(friends = originPerson.friends.toMutableList())
+        copyPerson.friends.add("suchan")
+
+        assertThat(originPerson.friends).hasSize(1)
+        assertThat(copyPerson.friends).hasSize(2)
+    }
+
+    data class MutablePerson(var name: String)
+
+    @Test
+    fun `p14 6번 예시`() {
+        val person = MutablePerson("Injin Jeong")
+        val map = mutableMapOf(person to "Developer")
+
+        println(map[person]) // "Developer" 출력
+        assertThat(map[person]).isEqualTo("Developer")
+
+        // 객체 상태 변경
+        person.name = "Jane"
+
+        // 상태 변경 후 해시 코드가 변경됨
+        println(map[person]) // null 출력
+        assertThat(map[person]).isNull()
+    }
+
 
     @Test
     fun p15_1() {
@@ -150,13 +259,83 @@ class Item1KtTest {
     }
 
     @Test
+    fun p16_1_again() {
+        val list1: MutableList<Int> = mutableListOf() // mutable list
+        var list2: List<Int> = listOf() // var 프로퍼티 선언
+
+        // 두 가지 모두 변경할 수 있다.
+        list1.add(1)
+        list2 = list2 + 1
+
+        // 두 가지 모두 다음과 같이 += 연산자를 사용할 수 있다. 실질적인 동작은 다르다.
+        list1 += 1 // list1.plusAssign(1)로 변경
+        list2 += 1 // list2 = list2.plus(1)로 변경
+    }
+
+    @Test
+    fun p16_2() {
+        var list = listOf<Int>()
+        for (i in 1..1000) {
+            thread {
+                list = list + i
+            }
+        }
+        Thread.sleep(1000)
+        println(list.size) // 1000 이 아닐 확률이 높다.
+
+        val list2 = mutableListOf<Int>()
+        for (i in 1..123456) {
+            thread {
+//                list2 += i
+                list2.add(i)
+            }
+        }
+        Thread.sleep(1000)
+        println(list2.size)
+    }
+
+    @Test
+    fun `16_2_test`() {
+        val list2 = mutableListOf<Int>()
+        val threads = mutableListOf<Thread>()
+
+        for (i in 1..12345) {
+            val thread = thread {
+                list2.add(i)
+            }
+            threads.add(thread)
+        }
+
+        threads.forEach { it.join() } // 모든 스레드가 작업을 마칠 때까지 대기
+
+        println("Expected size: 1000")
+        println("Actual size: ${list2.size}")
+
+    }
+
+    @Test
     fun p17_1() {
         var names by Delegates.observable(listOf<String>()) { _, old, new ->
+            println("====================================")
             println("Names changed from $old to $new")
         }
 
         names += "Fabio" // Names changed from [] to [Fabio]
         names += "Bill" // Names changed from [Fabio] to [Fabio, Bill]
+        println("====================================")
+
+        /**
+         * Delegates.observable은 프로퍼티의 참조나 값 자체의 변경을 감지하는 용도로 유용하지만,
+         * 리스트와 같은 컬렉션의 내용 변경을 직접 감지하지는 않습니다.
+         * 컬렉션의 내용 변경을 감지하려면 컬렉션을 래핑하거나 컬렉션의 변경 가능한 메서드를
+         * 오버라이딩하는 등의 추가적인 구현이 필요합니다.
+         */
+        var myList: MutableList<Int> by Delegates.observable(mutableListOf()) { prop, old, new ->
+            println("List has changed from $old to $new")
+        }
+
+        myList.add(1) // 리스트 변경
+        myList = mutableListOf(1, 2, 3) // 리스트 재할당
     }
 
     @Test
@@ -171,9 +350,42 @@ class Item1KtTest {
 
         val storedUsers2 = userRepository.loadAll2()
 //        storedUsers2[4] = "sososososo" //에러
-
     }
 
+    @Test
+    fun `Banking Property 패턴`() {
+        val inventory = Inventory()
+        val items = inventory.items
+//        items.add(Item("Sword")) // 에러
+        inventory.addItem(Item("Sword"))
+        inventory.addItem(Item("Shield"))
+        inventory.addItem(Item("Potion"))
+
+        println(inventory.items)
+        inventory.removeItem(inventory.items[1])
+        println(inventory.items)
+    }
+
+}
+
+class Item(val name: String)
+
+class Inventory {
+    // 클래스 내부에서 사용될 가변 리스트
+    private val _items = mutableListOf<Item>()
+
+    // 외부에 노출될 읽기 전용 리스트
+    val items: List<Item> get() = _items
+
+    // Item을 추가하는 메서드
+    fun addItem(item: Item) {
+        _items.add(item)
+    }
+
+    // Item을 제거하는 메서드
+    fun removeItem(item: Item) {
+        _items.remove(item)
+    }
 }
 
 data class User(val name: String, val surname: String)
@@ -189,5 +401,26 @@ class UserRepository {
     fun loadAll2(): Map<Int, String> {
         return storedUsers
 //        return storedUsers.toMap()
+    }
+}
+
+class p10Class {
+    val name: String? = "Injin"
+    val surname: String? = "Jeong"
+
+    val fullName: String?
+        get() = name?.let { "$it $surname" }
+
+    val fullName2: String? = name?.let { "$it $surname" }
+}
+
+fun p10Main() {
+    val p10 = p10Class()
+//    if (p10.fullName != null) {
+//        println(p10.fullName.length)
+//    }
+
+    if (p10.fullName2 != null) {
+        println(p10.fullName2.length)
     }
 }
