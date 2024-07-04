0ed80930264f637c881a65430e4ead1ffbc7a22e
JeongInjin <snowwwwww@naver.com>
2024-07-04 19:51:09 +0900

    effectiveKOtlin

--- src/test/kotlin/me/injin/effectiveKotlin/_04_/Item32.kt
+++ src/test/kotlin/me/injin/effectiveKotlin/_04_/Item32.kt
@@ -1,10 +1,32 @@
 package me.injin.effectiveKotlin._04_
 
+import org.junit.jupiter.api.Test
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.jvm.isAccessible
 
-class Item32
+class Item32 {
+    @Test
+    fun p_203_Test() {
+        val set = mutableSetOf(Item32_Id(1))
+        set.add(Item32_Id(1))
+        set.add(Item32_Id(1))
+        println("set.size: ${set.size}") // 3
+    }
+}
 
+/**
+ * equals 메서드는 오버라이드되었지만, hashCode 메서드는 오버라이드되지 않았습니다.
+ * Set은 중복 요소를 허용하지 않지만, 중복 여부를 판단하기 위해 equals와 hashCode를 모두 사용합니다.
+ */
+class Item32_Id(val id: Int) {
+    override fun equals(other: Any?): Boolean {
+        return other is Item32_Id && other.id == id
+    }
+
+//    override fun hashCode(): Int {
+//        return id
+//    }
+}
 
 /**
  * 리플렉션을 활용하여 클래스 외부에서 private 함수를 호출하는 예.
